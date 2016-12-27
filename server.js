9e72cda160b4da73a87591d63bba41ea1decda95
ravi <ravi.kumar127@wipro.com>
2016-12-27 14:53:16 +0000

    prep code to heroku

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require("express");
 //create  our app
 var app = express();
+const PORT=process.env.PORT || 8085;
 
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto']=='http'){
+    next();
+  }
+  else{
+    req.redirect('http://'+req.hostname + req.url);
+  }
+
+});
 app.use(express.static('public'));
 
-app.listen(8080, function () {
-  console.log("express server is up on port 8085");
+app.listen(PORT, function () {
+  console.log("express server is up on port"+PORT);
 });

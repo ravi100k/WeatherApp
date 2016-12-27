6cba9dbb22893fd1d16742abeae6485a5e8375f9
ravi kumar <ravi kumar>
2016-12-28 01:30:28 +0530

    try

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express = require("express");
-//create  our app
+var express = require('express');
+
+// Create our app
 var app = express();
-const PORT=process.env.PORT || 8085;
+const PORT = process.env.PORT || 8080;
 
-app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto']==='http'){
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
   }
-  else{
-    req.redirect('http://'+ req.hostname + req.url);
-  }
-
 });
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log("express server is up on port"+PORT);
+  console.log('Express server is up on port '+PORT);
 });

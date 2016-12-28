7a715fa28adbcd140de92b0285ab843cd6d5ff9d
ravi kumar <ravi kumar>
2016-12-28 13:26:03 +0530

    applid foundation to nav bar

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // var Example = React.createClass({
 //   render: function(){
 //     return(
@@ -13,7 +13,16 @@ var React = require('react');
 var Example = (props) =>{
   return(
     <div>
-      <h2>Example component!</h2>
+      <h2 className="text-center">Example!</h2>
+      <p>Here are some example to try out!</p>
+      <ol>
+        <li>
+          <Link to='/?location=Patna'>Patna, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio de Janeiro'>Rio de Janeiro, Brazil</Link>
+        </li>
+      </ol>
     </div>
   );
 }
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -16,7 +16,12 @@ var Main =(props) => {
   return(
     <div>
       <Nav/>
-      {props.children}
+      <div className="row">
+          <div className="columns medium-6 large-4 small-centered">
+            {props.children}
+          </div>
+      </div>
+
     </div>
   );
 };

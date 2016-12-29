7424b7f6d66306ae56efac5e28cad77079caf714
ravi kumar <ravi kumar>
2016-12-29 20:08:27 +0530

    modal box added

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,16 +1,5 @@
 var React = require('react');
 
-// var WeatherMessage = React.createClass({
-//   render: function(){
-//     var {location, temp} = this.props;
-//     return(
-//       <div>
-//         <h4>It's {temp} Degree Celcius in {location}</h4>
-//       </div>
-//     );
-//   }
-// });
-
 var WeatherMessage =({location, temp}) =>
 {
   return(
